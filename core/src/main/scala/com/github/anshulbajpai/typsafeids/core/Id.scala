2512b38c93c793cc72c99721e468ca3964974779
Anshul Bajpai <bajpai.anshul@gmail.com>
2018-06-08 00:38:48 +0100

    Fixing compilation errors

--- core/src/main/scala/com/github/anshulbajpai/typsafeids/core/Id.scala
+++ core/src/main/scala/com/github/anshulbajpai/typsafeids/core/Id.scala
@@ -4,5 +4,4 @@ case class Id[A <: IdType](value: A#IdValue) extends AnyVal
 
 object Id {
   def apply[T <: IdType](implicit generator: IdValueGenerator[T]): Id[T] = apply(generator.generate)
-  def apply[T <: IdType](newValue: T#IdValue): Id[T] = new Id[T](newValue)
 }
--- json/play/src/main/scala/com/github/anshulbajpai/typesafeids/json/play/implicits.scala
+++ json/play/src/main/scala/com/github/anshulbajpai/typesafeids/json/play/implicits.scala
@@ -5,11 +5,13 @@ import play.api.libs.json._
 
 object implicits {
 
-  implicit def reads[T <: IdType](implicit idValueReads: Reads[T#IdValue]): Reads[Id[T]] =
-    (json: JsValue) => idValueReads.reads(json).map(Id[T])
+  implicit def reads[T <: IdType](implicit idValueReads: Reads[T#IdValue]): Reads[Id[T]] = new Reads[Id[T]] {
+    override def reads(json: JsValue): JsResult[Id[T]] = idValueReads.reads(json).map((value: T#IdValue) => Id[T](value))
+  }
 
-  implicit def writes[T <: IdType](implicit idValueWrites: Writes[T#IdValue]): Writes[Id[T]] =
-    (o: Id[T]) => idValueWrites.writes(o.value)
+  implicit def writes[T <: IdType](implicit idValueWrites: Writes[T#IdValue]): Writes[Id[T]] = new Writes[Id[T]] {
+    override def writes(o: Id[T]): JsValue = idValueWrites.writes(o.value)
+  }
 
   implicit def format[T <: IdType](implicit idValueReads: Reads[T#IdValue], idValueWrites: Writes[T#IdValue]): Format[Id[T]] =
     Format(reads, writes)
